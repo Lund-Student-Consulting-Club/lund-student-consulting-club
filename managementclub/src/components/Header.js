afb1d0d5964adbc8d7ebbae56b74eb3107fb9ae2
daniel <danielwm76@gmail.com>
2024-08-01 03:05:32 +0200

    fixa bilder o skit

--- managementclub/src/components/Header.js
+++ managementclub/src/components/Header.js
@@ -6,6 +6,7 @@ const Header = () => {
     <header>
       <nav>
         <ul>
+          <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
           <li><Link to="/contact">Contact</Link></li>
         </ul>
--- managementclub/src/components/Home.js
+++ managementclub/src/components/Home.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import './Home.css'; // Import the CSS file for styling
+import './Home.css'; // Import the CSS file for stylin
+import CardSection from './CardSection'; //
 
 const Home = () => {
   return (
-    <div className="home-container">
-      <div className="overlay-text">
-        <h1>Welcome to Our Organization</h1>
-        <p>This is a brief introduction to our organization.</p>
+    <>
+      <div className="home-container">
+        <div className="overlay-text">
+          <h1>Welcome to Our Organization</h1>
+          <p>This is a brief introduction to our organization.</p>
+        </div>
       </div>
-    </div>
+      <CardSection />
+    </>
   );
 };
 
