d0b9339f594be03829e9272f9ac8b3ff41aab2ba
Daniel Lai Wikström <daniellw@kth.se>
2024-08-30 17:33:08 +0200

    remove unused vars

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
 import Footer from './components/Footer';
-import { ScrollProvider, ScrollContext } from './components/ScrollContext';
+import { ScrollProvider } from './components/ScrollContext';
 
 function App() {
   
